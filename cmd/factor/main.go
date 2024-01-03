567035c903cec94cb0ef37809aef1d449e5e71af
Гузеев Иван Михайлович <ivan.guzeev@sbermegamarket.ru>
2024-01-04 02:30:50 +0300

    add some logs

--- cmd/factor/main.go
+++ cmd/factor/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	logrus.Info("hello world")
+
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		logrus.Fatal("port is empty")
+	}
+
+}
