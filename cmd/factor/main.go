88fcdfba692f22cbf943f708c608e49c237d0590
Гузеев Иван Михайлович <ivan.guzeev@sbermegamarket.ru>
2024-01-04 14:01:32 +0300

    add http.Server, mux

--- cmd/factor/main.go
+++ cmd/factor/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
+	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
 )
 
@@ -15,10 +21,24 @@ func main() {
 		logrus.Fatal("port is empty")
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	})
 
-	addr := ":" + port
-	http.ListenAndServe(addr, nil)
+	serv := http.Server{
+		Addr:    net.JoinHostPort("", port),
+		Handler: router,
+	}
+
+	go serv.ListenAndServe()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM) // kill -TERM <pid>
+
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	serv.Shutdown(ctx)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/hisnameisivan/demo_12factor
 go 1.20
 
 require (
-	github.com/sirupsen/logrus v1.9.3 // indirect
-	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	github.com/gorilla/mux v1.8.1
+	github.com/sirupsen/logrus v1.9.3
 )
+
+require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
